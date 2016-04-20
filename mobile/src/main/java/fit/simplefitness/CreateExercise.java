11a370e6814cf048005708879e6b8929abaab046
Dalton Ellis <daltonellis@pc.local>
2016-04-20 18:35:38 -0500

    Set up the activities to add exercises, before creating db

--- mobile/src/main/java/fit/simplefitness/CreateExercise.java
+++ mobile/src/main/java/fit/simplefitness/CreateExercise.java
@@ -1,14 +1,17 @@
 package fit.simplefitness;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateExercise extends AppCompatActivity {
-
+    private static final String TAG = "CreateExerciseActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +19,32 @@ public class CreateExercise extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_exercise);
         fab.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                final EditText taskEditText = new EditText(view.getContext());
+                AlertDialog dialog = new AlertDialog.Builder(view.getContext())
+                        .setTitle("Add a new exercise")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Exercise to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+
+
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
