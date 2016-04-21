3ac87b5c4cba497bfa287ffc5b7bb6288888c237
Dalton Ellis <daltonellis@pc.local>
2016-04-20 19:23:21 -0500

    Added linear layout to add multiple editText values to alert dialog

--- mobile/src/main/java/fit/simplefitness/CreateExercise.java
+++ mobile/src/main/java/fit/simplefitness/CreateExercise.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class CreateExercise extends AppCompatActivity {
     private static final String TAG = "CreateExerciseActivity";
@@ -29,15 +31,40 @@ public class CreateExercise extends AppCompatActivity {
             public void onClick(View view) {
 
                 final EditText taskEditText = new EditText(view.getContext());
+                final EditText taskNumReps = new EditText(view.getContext());
+                final EditText taskNumSets = new EditText(view.getContext());
+                final EditText taskNumMiles = new EditText(view.getContext());
+                taskNumReps.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                taskNumSets.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                taskNumMiles.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                taskEditText.setHint("Name of Exercise");
+                taskNumReps.setHint("Number of Reps");
+                taskNumMiles.setHint("Number of Miles");
+                taskNumSets.setHint("Number of Sets");
+
+
+                LinearLayout ll = new LinearLayout(view.getContext());
+                ll.setOrientation(LinearLayout.VERTICAL);
+                ll.addView(taskEditText);
+                ll.addView(taskNumMiles);
+                ll.addView(taskNumSets);
+                ll.addView(taskNumReps);
+
                 AlertDialog dialog = new AlertDialog.Builder(view.getContext())
                         .setTitle("Add a new exercise")
-                        .setMessage("What do you want to do next?")
-                        .setView(taskEditText)
+                        .setMessage("Enter all that apply")
+                        .setView(ll)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 String task = String.valueOf(taskEditText.getText());
+                                String reps = String.valueOf(taskNumReps.getText());
+                                String miles = String.valueOf(taskNumMiles.getText());
+                                String sets = String.valueOf(taskNumSets.getText());
                                 Log.d(TAG, "Exercise to add: " + task);
+                                Log.d(TAG, "Number of miles: " + miles);
+                                Log.d(TAG, "Number of reps: " + reps);
+                                Log.d(TAG, "Number of sets: " + sets);
                             }
                         })
                         .setNegativeButton("Cancel", null)
