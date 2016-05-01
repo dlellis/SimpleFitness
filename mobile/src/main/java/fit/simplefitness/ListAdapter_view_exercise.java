2dc1aaa9f67a2cf3d279ab0b687426fc38a1f233
Dalton Ellis <daltonellis@pc.local>
2016-04-30 19:33:06 -0500

    Updated gradle. Corrected annoying functionality issues.

---
+++ mobile/src/main/java/fit/simplefitness/ListAdapter_view_exercise.java
@@ -0,0 +1,51 @@
+package fit.simplefitness;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fit.simplefitness.models.Exercise;
+
+
+public class ListAdapter_view_exercise extends ArrayAdapter<Exercise> {
+
+    public ListAdapter_view_exercise(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+
+    public ListAdapter_view_exercise(Context context, int resource, List<Exercise> items) {
+        super(context, resource, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.exercises, null);
+        }
+
+        Exercise p = getItem(position);
+
+        if (p != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.exercise_name);
+
+
+            if (tt1 != null) {
+                tt1.setText(p.name);
+
+            }
+
+        }
+            return v;
+        }
+
+    }
--- mobile/src/main/java/fit/simplefitness/ViewWorkout.java
+++ mobile/src/main/java/fit/simplefitness/ViewWorkout.java
@@ -1,7 +1,11 @@
 package fit.simplefitness;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ViewWorkout extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class ViewWorkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_workout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
