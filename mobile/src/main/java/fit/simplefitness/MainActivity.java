48c434b0138d6f23a0d3f4cbd305b1dde51743f2
Dalton Ellis <daltonellis@pc.local>
2016-05-04 14:52:48 -0500

    First successful transfer of data

--- mobile/src/main/java/fit/simplefitness/MainActivity.java
+++ mobile/src/main/java/fit/simplefitness/MainActivity.java
@@ -10,21 +10,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.MessageEvent;
 import com.orm.query.Select;
 
 import java.util.List;
 
 import fit.simplefitness.models.Workout;
+import jp.android.a.akira.library.okwear.OkWear;
+import jp.android.a.akira.library.okwear.listener.SendResultListener;
+import jp.android.a.akira.library.okwear.listener.WearReceiveListener;
+import jp.android.a.akira.library.okwear.util.Payload;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WearReceiveListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public final static String extra_message = "worked";
     public Integer newint;
-
-
     private ListView mWkListView;
-    public void createExercise(View view){
+    private OkWear mOkWear;
+    private int mPayload = 0;
+
+    public void createExercise(View view) {
         Intent intent = new Intent(this, CreateWorkout.class);
 
         startActivity(intent);
@@ -36,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         UpdateUI();
+
+        mOkWear = new OkWear(this);
+        mOkWear.registReceiver(this);
+
     }
 
 
@@ -49,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
     public void viewWorkout(View view) {
         View parentRow = (View) view.getParent();
         ListView listView = (ListView) parentRow.getParent();
@@ -81,19 +97,65 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void UpdateUI(){
+    private void UpdateUI() {
         try {
             List<Workout> exercises = Select.from(Workout.class).orderBy("id").list();
             mWkListView = (ListView) findViewById(R.id.workout_lView);
             android.widget.ListAdapter mAdapter = new ListAdapter_workout(this, R.layout.content_workout, exercises);
             mWkListView.setAdapter(mAdapter);
 
-        }
-        catch (SQLiteException e) {
+        } catch (SQLiteException e) {
             return;
         }
 
     }
 
+    public void get_workouts(){
 
+    }
+
+    private void sampleSyncData() {
+        final Payload payload =
+                new Payload.Builder(OkWear.DEFAULT_DATA_API_PATH)
+                        .addPayload(OkWear.DEFAULT_DATA_API_KEY, mPayload++)
+                        .addPayload("my key", "hello")
+                        .build();
+
+        mOkWear.syncData(payload, new SendResultListener<DataApi.DataItemResult>() {
+            @Override
+            public void onResult(DataApi.DataItemResult result) {
+                Log.v(TAG, "Status: " + result.getStatus());
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mOkWear.connect();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mOkWear.disconnect();
+    }
+
+    @Override
+    public void onReceiveMessage(MessageEvent messageEvent) {
+        if (messageEvent.getPath().equals(OkWear.DEFAULT_MESSAGE_API_PATH)) {
+            final String messagePayload = new String(messageEvent.getData());
+            Log.v(TAG, messagePayload);
+        }
+    }
+
+    @Override
+    public void onReceiveDataApi(DataEventBuffer dataEventBuffer) {
+        for (DataEvent event : dataEventBuffer) {
+            DataMap dataMap = DataMap.fromByteArray(event.getDataItem().getData());
+            final int data = dataMap.getInt(OkWear.DEFAULT_DATA_API_KEY);
+            Log.v(TAG, "data: " + data);
+        }
+
+    }
 }
--- wear/src/main/java/fit/simplefitness/MainActivity.java
+++ wear/src/main/java/fit/simplefitness/MainActivity.java
@@ -3,28 +3,46 @@ package fit.simplefitness;
 /**
  * Created by daltonellis on 5/3/16.
  */
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.view.WearableListView;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import fit.simplefitness.listview.ListViewAdapter;
 import fit.simplefitness.listview.ListViewItem;
+import jp.android.a.akira.library.okwear.OkWear;
+import jp.android.a.akira.library.okwear.listener.NodeChangeListener;
+import jp.android.a.akira.library.okwear.listener.SendResultListener;
+import jp.android.a.akira.library.okwear.util.ParseByteArray;
+import jp.android.a.akira.library.okwear.util.Payload;
 
 
-public class MainActivity extends Activity implements WearableListView.ClickListener{
+public class MainActivity extends Activity implements WearableListView.ClickListener, View.OnClickListener{
 
     private List<ListViewItem> viewItemList = new ArrayList<>();
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mSendButton;
+    private OkWear mOkWear;
+    private int mPayload = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WearableListView wearableListView = (WearableListView) findViewById(R.id.wearable_list_view);
+       WearableListView wearableListView = (WearableListView) findViewById(R.id.wearable_list_view);
 
         viewItemList.add(new ListViewItem(R.drawable.ic_basketball, "Basketball"));
         viewItemList.add(new ListViewItem(R.drawable.ic_baseball, "Baseball"));
@@ -33,15 +51,98 @@ public class MainActivity extends Activity implements WearableListView.ClickList
 
         wearableListView.setAdapter(new ListViewAdapter(this, viewItemList));
         wearableListView.setClickListener(this);
+
+        mOkWear = new OkWear(this);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        //Log.d("watchstube", "first");
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                //Log.d("watchstube","second");
+                mSendButton = (Button) stub.findViewById(R.id.activity_main_send_button);
+                mSendButton.setOnClickListener(MainActivity.this);
+            }
+        });
+
     }
 
     @Override
     public void onClick(WearableListView.ViewHolder viewHolder) {
-        Toast.makeText(this, "Click on " + viewItemList.get(viewHolder.getPosition()).text, Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "Click on " + viewItemList.get(viewHolder.getPosition()).text, Toast.LENGTH_SHORT).show();
+        sampleSendMessageAllAsync();
     }
 
     @Override
     public void onTopEmptyRegionClick() {
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mOkWear.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mOkWear.disconnect();
+    }
+
+    @Override
+   public void onClick(View v) {
+       //sampleSendData();
+        //sampleSendMessageAll();
+        //sampleSendMessageAllAsync();
+    }
+
+    private void sampleSendMessage() {
+        final byte[] bs = ParseByteArray.fromInt(100);
+
+        mOkWear.getNodeList(new NodeChangeListener() {
+            @Override
+            public void onReceiveNodes(List<Node> nodes) {
+                mOkWear.sendMessage(nodes.get(0), bs, null, null);
+            }
+        });
+    }
+
+    private void sampleSendMessageAll() {
+        mOkWear.sendMessageAll("hello message", null);
+    }
+
+    private void sampleSendMessageAllAsync() {
+        final byte[] bs = ("hello message").getBytes();
+        mOkWear.sendMessageAllAsync(bs, null, new SendResultListener<MessageApi.SendMessageResult>() {
+            @Override
+            public void onResult(MessageApi.SendMessageResult result) {
+                Log.v(TAG, "Status: " + result.getStatus());
+            }
+        });
+    }
+
+    private void sampleSyncData() {
+        final Payload payload =
+                new Payload.Builder(OkWear.DEFAULT_DATA_API_PATH)
+                        .addPayload(OkWear.DEFAULT_DATA_API_KEY, mPayload++)
+                        .addPayload("my key", "hello")
+                        .build();
+
+        mOkWear.syncData(payload, new SendResultListener<DataApi.DataItemResult>() {
+            @Override
+            public void onResult(DataApi.DataItemResult result) {
+                Log.v(TAG, "Status: " + result.getStatus());
+            }
+        });
+    }
+
+    private void sampleSyncData2() {
+
+        mOkWear.syncData(OkWear.DEFAULT_DATA_API_PATH,
+                OkWear.DEFAULT_DATA_API_KEY,
+                "hello");
+    }
+
+
 }
